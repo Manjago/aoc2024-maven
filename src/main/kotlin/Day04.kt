9a9f5eb86e74de1ea6a1e9fd3c4601785b64f59a
Темненков Кирилл Дмитриевич <kirill.temnenkov@mts.ru>
2024-12-04 17:41:06 +0300

    Day4 part2

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -2,6 +2,31 @@ fun main() {
 
     fun Array<String>.inBounds(x: Int, y: Int, width: Int, height: Int): Boolean = x in 0..width - 1 && y in 0..height - 1
 
+    fun Array<String>.checkX(x: Int, y: Int): Boolean {
+        val width = this[0].length
+        val height = this.size
+
+
+        if (this.inBounds(x+1, y+1, width, height) &&
+            this.inBounds(x-1, y-1, width, height) &&
+            this.inBounds(x+1, y-1, width, height) &&
+            this.inBounds(x-1, y+1, width, height)
+        ) {
+             val first = this[y + 1][x + 1]
+             val twinFirst = this[y - 1][x - 1]
+
+             val second = this[y - 1][x + 1]
+             val twinSecond = this[y + 1][x - 1]
+
+            val possibleGood1 = (first == 'M' && twinFirst == 'S') || (first == 'S' && twinFirst == 'M')
+            val possibleGood2 = (second == 'M' && twinSecond == 'S') || (second == 'S' && twinSecond == 'M')
+            if (possibleGood1 && possibleGood2) return true
+        }
+
+
+        return false
+    }
+
     fun Array<String>.check(x: Int, y: Int, shiftX: Int, shiftY: Int) : Boolean {
         val width = this[0].length
         val height = this.size
@@ -88,7 +113,28 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 1
+        val data: Array<String> = Array(input[0].length) { "" }
+        for(y in input.size - 1 downTo 0) {
+            data[y] = input[y]
+        }
+
+        val width = input[0].length
+        val height = input.size
+        var answer = 0
+
+        for(y in 0 until height) {
+            for (x in 0 until width) {
+                val char = data[y][x]
+                if (char != 'A') {
+                    continue
+                }
+                if (data.checkX(x, y)) {
+                    ++answer
+                }
+            }
+        }
+
+        return answer
     }
 
     val testInput = readInput("Day04_test")
@@ -96,10 +142,10 @@ fun main() {
     check(testPart1 == 18) { "Expected 1 but got $testPart1" }
 
     val input = readInput("Day04")
-    part1(input).println()
-/*
+    val part1 = part1(input)
+    part1.println()
+    check(part1 == 2496) { "Expected 2496 but got $testPart1" }
     val testPart2 = part2(testInput)
-    check(testPart2 == 1) { "Expected 1 but got $testPart2" }
+    check(testPart2 == 9) { "Expected 9 but got $testPart2" }
     part2(input).println()
-*/
 }

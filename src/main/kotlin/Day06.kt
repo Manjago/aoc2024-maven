eb452b1f2ce3c847774b44b4de349452b5d29856
Темненков Кирилл Дмитриевич <kirill.temnenkov@mts.ru>
2024-12-06 14:14:59 +0300

    Day6 part2

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -2,20 +2,32 @@ import Direction.NORTH
 
 fun main() {
 
-    class Maze(val width: Int, val height: Int) {
-        private val obstacles = mutableSetOf<SimplePoint>()
+    class Maze(
+        val width: Int,
+        val height: Int,
+        private val obstacles: MutableSet<SimplePoint> = mutableSetOf<SimplePoint>()
+    ) {
 
         fun addObstacle(point: SimplePoint) {
             obstacles.add(point)
         }
 
         fun hasObstacle(point: OrientedPoint): Boolean {
-            return obstacles.contains(point.toSimplePoint())
+            return hasObstacle(point.toSimplePoint())
         }
 
-        fun inMe(point: OrientedPoint): Boolean {
-            return point.x in 0..width - 1 && point.y in 0..height - 1
-        }
+        fun hasObstacle(point: SimplePoint): Boolean = obstacles.contains(point)
+
+        fun isFree(point: SimplePoint): Boolean = !hasObstacle(point) && inMe(point)
+
+        private fun inMe(x: Int, y: Int): Boolean = x in 0..width - 1 && y in 0..height - 1
+
+        fun inMe(point: OrientedPoint): Boolean = inMe(point.x, point.y)
+
+        fun inMe(point: SimplePoint): Boolean = inMe(point.x, point.y)
+
+        fun withObstacle(point: SimplePoint): Maze =
+            Maze(width, height, obstacles.toMutableSet()).apply { this.addObstacle(point) }
 
         override fun toString(): String {
             return "Maze(width=$width, height=$height, obstacles=$obstacles)"
@@ -55,8 +67,39 @@ fun main() {
         return seen.size
     }
 
+    fun hasLoop(maze: Maze, from: OrientedPoint): Boolean {
+        var current = from
+        val seen = mutableSetOf<OrientedPoint>()
+        while(maze.inMe(current)) {
+            if (seen.contains(current)) {
+                return true
+            }
+            seen.add(current)
+
+            val nextStep = current.step()
+            if (maze.hasObstacle(nextStep)) {
+                current = current.turnRight()
+            } else {
+                current = nextStep
+            }
+
+        }
+        return false
+    }
+
     fun part2(input: List<String>): Int {
-        return 1
+
+        val (maze, pos) = parseMaze(input)
+
+        var answer = 0
+        for (y in 0 until maze.height) for (x in 0 until maze.width) {
+            val currentPoint = SimplePoint(x, y)
+            if (maze.isFree(currentPoint) && hasLoop(maze.withObstacle(currentPoint), pos)) {
+                ++answer
+            }
+        }
+
+        return answer
     }
 
     val testInput = readInput("Day06_test")
@@ -68,6 +111,6 @@ fun main() {
     part1.println()
     check(part1 == 5095) { "Expected 5095 but got $part1" }
     val testPart2 = part2(testInput)
-    check(testPart2 == 1) { "Expected 1 but got $testPart2" }
+    check(testPart2 == 6) { "Expected 6 but got $testPart2" }
     part2(input).println()
 }
